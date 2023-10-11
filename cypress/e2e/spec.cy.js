f936f595fa7d34e5599b411af4268da20ff3d960
thashaa <107286289+thashaa@users.noreply.github.com>
2023-10-11 15:32:10 +0700

    second commit

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,4 +2,9 @@ describe('My First Test', () => {
   it('Does not do much!!', () => {
     expect(false).to.equal(false)
   })
+}) 
+describe('My second loop', () => {
+  it('Does!', () => {
+    expect(false).to.equal(false)
+  })
 })  
